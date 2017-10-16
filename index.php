a25f9ad76633c7eb2dcb1b4353b63f98cc78b27b
agreyesaldeco <agreyesaldeco@gmail.com>
2017-10-16 11:38:33 -0500

    index nuevo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!--script src="_js/v3.14.1/ol.js" type="text/javascript"></script!-->
+
+		<TITLE>Seleccion de Servicios</TITLE>
+		<style>
+			
+			html, body{
+			      width:100%;
+			      height:100%;
+			      padding:0;
+			      margin:0;
+			      background:white;
+			      text-align:center;
+			}
+		</style>
+	</head>
+	<body  oncontextmenu="return false" >
+		<h1>Sistema Fuera de Servicio</h1>
+		 <h1>por mantenimiento</h1>
+	</body>
+</html>
+
+
